c5b684b153fbaeb9032a602a9ac3217e53580428
Andrey Berrocal Cuadra <andrey7cr@gmail.com>
2025-03-22 14:50:43 -0600

    Se añade resumen de gastos e ingresos.

--- GestorFinanzasMVC/Controllers/FinanzasController .cs
+++ GestorFinanzasMVC/Controllers/FinanzasController .cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using GestorFinanzasMVC.Models;
 
 namespace GestorFinanzasMVC.Controllers
 {
     public class FinanzasController : Controller
     {
+        private readonly GestorFinanzasDbContext _context;
+
+        public FinanzasController(GestorFinanzasDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Registrar()
         {
             return View();
@@ -12,6 +23,27 @@ namespace GestorFinanzasMVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Resumen()
+        {
+            var usuarioId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var ingresos = await _context.Ingresos
+                .Where(i => i.UsuarioId == usuarioId)
+                .Include(i => i.Categoria)
+                .OrderByDescending(i => i.Fecha)
+                .ToListAsync();
+
+            var gastos = await _context.Gastos
+                .Where(g => g.UsuarioId == usuarioId)
+                .Include(g => g.Categoria)
+                .OrderByDescending(g => g.Fecha)
+                .ToListAsync();
+
+            return View("Resumen", ((IEnumerable<Ingreso>)ingresos, (IEnumerable<Gasto>)gastos));
+
         }
     }
 }
